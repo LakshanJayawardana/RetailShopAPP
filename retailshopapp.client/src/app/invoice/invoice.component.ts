fd40b2a27f9a0ee60bede5c5e58a517db356da6f
PEROOT\VJAAYLA <lakshan.jaayawardana@pearson.com>
2025-03-30 09:47:20 +0530

    Update invoice details

--- retailshopapp.client/src/app/invoice/invoice.component.ts
+++ retailshopapp.client/src/app/invoice/invoice.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { InvoiceService } from '../invoice.service';
+import { InvoiceService } from '../shared/invoice.service';
+import { NgForm } from '@angular/forms';
+import { InvoiceModel } from '../shared/invoice.model';
 
 @Component({
   selector: 'app-invoice',
@@ -8,8 +10,8 @@ import { InvoiceService } from '../invoice.service';
   styleUrls: ['./invoice.component.css']
 })
 export class InvoiceComponent {
-  
   constructor(private invoiceService: InvoiceService, private formBuilder: FormBuilder) { }
+
   ngOnInit() {
     this.invoiceService.getInvoice().subscribe((data: any) => {
       this.invoiceService.invoice = data;
@@ -18,8 +20,27 @@ export class InvoiceComponent {
 
     });
   }
+  
 
-  onSubmit() {
-    
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      if (this.invoiceService.formData.id == 0)
+        this.insertRecord(form);
+      else
+        this.updateRecord(form);
+    }
   }
+  insertRecord(form: NgForm) {
+    this.invoiceService.addInvoice()
+      .subscribe({
+      next: res => {
+        this.invoiceService.invoice = res as InvoiceModel[];
+
+      },
+      error: err => {
+        console.log(err);
+      }
+    })
+  }
+  updateRecord(form: NgForm) { }
 }
