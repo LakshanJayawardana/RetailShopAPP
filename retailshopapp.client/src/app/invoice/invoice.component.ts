3a28ccb9a1858338df8c5ccf2ab16140583ec25c
PEROOT\VJAAYLA <lakshan.jaayawardana@pearson.com>
2025-03-30 15:01:34 +0530

    Frontend view data binding

--- retailshopapp.client/src/app/invoice/invoice.component.ts
+++ retailshopapp.client/src/app/invoice/invoice.component.ts
@@ -2,7 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { InvoiceService } from '../shared/invoice.service';
 import { NgForm } from '@angular/forms';
-import { InvoiceModel } from '../shared/invoice.model';
+import { Invoice } from '../shared/invoice.model';
 
 @Component({
   selector: 'app-invoice',
@@ -10,37 +10,79 @@ import { InvoiceModel } from '../shared/invoice.model';
   styleUrls: ['./invoice.component.css']
 })
 export class InvoiceComponent {
+  recentInvoice: Invoice = {
+    invoiceId: 0,
+    transactionDate: new Date().toISOString(),
+    totalAmount: 0,
+    discount: 0,
+    balanceAmount: 0,
+    products: []
+  };;
+  formData: Invoice = {
+    invoiceId: 0,
+    transactionDate: new Date().toISOString(),
+    totalAmount: 0,
+    discount: 0,
+    balanceAmount: 0,
+    products: []
+  };
+  submitted: boolean = false;
+
   constructor(private invoiceService: InvoiceService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.invoiceService.getInvoice().subscribe((data: any) => {
-      this.invoiceService.invoice = data;
-      console.log(this.invoiceService.invoice);
-      console.log(data);
 
-    });
   }
-  
+
 
   onSubmit(form: NgForm) {
     if (form.valid) {
-      if (this.invoiceService.formData.id == 0)
-        this.insertRecord(form);
-      else
-        this.updateRecord(form);
+      if (this.formData.totalAmount > 0) {
+        if (this.formData.invoiceId === 0) {
+          this.submitted = true;
+          this.insertRecord(form);
+        }
+        else
+          this.updateRecord(form);
+      }
+      else {
+        alert("Please enter valid data");
+      }
+
     }
+
   }
   insertRecord(form: NgForm) {
-    this.invoiceService.addInvoice()
+    this.invoiceService.addInvoice(this.formData)
       .subscribe({
-      next: res => {
-        this.invoiceService.invoice = res as InvoiceModel[];
+        next: (res) => {
+          this.recentInvoice = res as Invoice;
+          console.log('Submitted Invoice:', this.recentInvoice);
+          this.resetForm(form);
+        },
+        error: err => {
+          console.error('Error occurred:', err);
+          if (err.error) {
+            console.error("Error details:", err.error);
+          }
+        }
+      });
 
-      },
-      error: err => {
-        console.log(err);
-      }
-    })
   }
-  updateRecord(form: NgForm) { }
+  updateRecord(form: NgForm) {
+  }
+  getInvoiceDetails() {
+    this.invoiceService.getInvoice().subscribe((data: any) => {
+      const sortedInvoice = data.sort((a: Invoice, b: Invoice) =>
+        new Date(b.transactionDate).getTime() - new Date(a.transactionDate).getTime());
+      const mostRecentInvoice = sortedInvoice[0];
+      this.formData = mostRecentInvoice;
+      console.log(this.formData);
+    });
+  }
+  resetForm(form?:NgForm) {
+    if (form) {
+      form.resetForm();
+    }
+  }
 }
--- retailshopapp.client/src/app/shared/invoice.model.ts
+++ retailshopapp.client/src/app/shared/invoice.model.ts
@@ -1,3 +1,15 @@
-export class InvoiceModel {
-  id: number = 0;
+export class Product {
+  productId: number = 0;
+  name: string ='';
+  price: number = 0;
+  quantity: number = 0;
+}
+
+export class Invoice {
+  invoiceId: number = 0;
+  transactionDate: string = '';  // Use string to handle date format
+  totalAmount: number = 0;
+  discount: number = 0;
+  balanceAmount: number = 0;
+  products: Product[] = [];  // List of products associated with the invoice
 }
